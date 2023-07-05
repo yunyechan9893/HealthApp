fc3a0fd942dba87856b5ae23b74e0592e9ec0cee
yunyechan9893 <dpcks9893@naver.com>
2023-07-05 17:18:00 +0900

    feat: 레트로핏 중복확인 구현

--- backend/app_v1/signup.py
+++ backend/app_v1/signup.py
@@ -1,28 +1,40 @@
 from app_v1 import api
-from flask import request
+from flask import request, jsonify
 import models
 
-@api.route('/signup', methods= ['POST', 'GET'])
+@api.route('/signup', methods= ['POST'])
 def signup():
-    if request.method == 'POST':
-        params = request.get_json()
-        user_id        = params['userId']
-        user_pwd       = params['userPwd'] 
-        user_name      = params['userName']
-        user_phone     = params['userPhone']
-        user_nickname  = params['userNickname']
-        user_position  = params['userPosition']
+    params = request.get_json()
+    user_id = params.get('userId')
+    user_pwd = params.get('userPwd')
+    user_name = params.get('userName')
+    user_phone = params.get('userPhone')
+    user_nickname = params.get('userNickname')
+    user_position = params.get('userPosition')
 
-        msg = models.register( 
-            user_id, 
-            user_pwd, 
-            user_name,
-            user_phone,
-            user_nickname, 
-            user_position 
-        )
-        
-        return 'True' if msg==True else 'False'
-    return 'False'
+    msg = models.register( 
+        user_id, 
+        user_pwd, 
+        user_name,
+        user_phone,
+        user_nickname, 
+        user_position 
+    )
+    
+    success = True if msg else False
 
-        
+    return jsonify({"success": success, "message": 200})
+
+@api.route('/duplicate-check/id/<id>', methods=['GET'])
+def duplicate_check_id(id):
+    
+    msg = models.get_id(id)
+    success = False if msg else True
+    return jsonify({"success": success, "message": 200})
+
+@api.route('/duplicate-check/nickname/<nickname>', methods=['GET'])
+def duplicate_check_nickname(user_nickname):
+    msg = models.get_nickname(user_nickname)
+    success = False if msg else True
+    return jsonify({"success": success, "message": 200})
+
