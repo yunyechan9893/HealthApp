841056fd4ca56a81eec863b3a8372776ca820332
yunyechan9893 <dpcks9893@naver.com>
2023-07-10 17:56:40 +0900

    Feat: 싱글톤 Room 추상클래스 구현

---
+++ Health/app/src/main/java/kr/ac/doowon/healthmanageapp/database/AppDatabase.java
@@ -0,0 +1,39 @@
+package kr.ac.doowon.healthmanageapp.database;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+@Database(entities = {Table.Diet.class, Table.AteFood.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract TableDAO.DietDAO dietDAO();
+    public abstract TableDAO.AteFoodDAO ateFoodDAO();
+
+    private static volatile AppDatabase INSTANCE;
+
+    private static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+
+        }
+    };
+
+    public static AppDatabase getDatabase(final Context context) {  // Singleton 패턴 구현
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    AppDatabase.class, "health_database")
+                            .addMigrations(MIGRATION_1_2)    // Migration 방법 지정
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
