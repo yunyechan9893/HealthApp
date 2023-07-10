f1ffca63938cf7dbe8f67878f338f3b7b216b906
yunyechan9893 <dpcks9893@naver.com>
2023-07-10 18:40:32 +0900

    Feat: add a Diet Table, a AteFood Table

--- backend/models/table.py
+++ backend/models/table.py
@@ -30,5 +30,71 @@ class User(Base):
         return self.position
     
 
+class Diet(Base):
+    __tablename__='Diet'
+    no         = Column(int, primary_key=True, autoincrement=True)
+    member_id   = Column(String(64), ForeignKey('Members.id'))
+    type_of_meal = Column(String(10))
+    meal_time   = Column(String(8))
+    comment    = Column(String(500))
+    date       = Column(String(10))
+    share      = Column(int)
+    url        = Column(String(500))
+
+    def get_type_of_meal(self):
+        return self.typeOfMeal
+    
+    def get_meal_time(self):
+        return self.meal_time
+    
+    def get_comment(self):
+        return self.comment
+    
+    def get_date(self):
+        return self.date
+    
+    def get_url(self):
+        return self.url
+    
+
+class AteFood(Base):
+    __tablename__='AteFood'
+    no            = Column(int, primary_key=True, autoincrement=True)
+    diet_no       = Column(int, ForeignKey('Diet.no'))
+    serial_number = Column(int)
+    food_Name     = Column(String(100))
+    amount        = Column(int)
+    kcal          = Column(int)
+    carbohydrate  = Column(int)
+    protein       = Column(int)
+    fat           = Column(int)
+    sodium        = Column(int)
+
+    def get_diet_no(self):
+        return self.diet_no
+
+    def get_serial_number(self):
+        return self.typeOfMeal
+    
+    def get_food_Name(self):
+        return self.food_Name
+    
+    def get_amount(self):
+        return self.amount
+    
+    def get_kcal(self):
+        return self.kcal
+    
+    def get_carbohydrate(self):
+        return self.carbohydrate
+
+    def get_protein(self):
+        return self.protein
+    
+    def get_fat(self):
+        return self.fat
+    
+    def get_sodium(self):
+        return self.sodium
 
     
