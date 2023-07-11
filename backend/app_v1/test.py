9864fa16520f928d73d4baf40310d2171881db17
yunyechan9893 <dpcks9893@naver.com>
2023-07-11 20:15:14 +0900

    Test: test code 작성 backend

--- backend/app_v1/test.py
+++ backend/app_v1/test.py
@@ -1,10 +1,29 @@
 from app_v1 import api
-from flask import request
+from flask import request, jsonify, current_app
+import logging
 import models
+
+def tool(msg, form=''):
+    ###
+    # 로깅 레벨 설정
+    current_app.logger.setLevel(logging.DEBUG)
+
+    # 로깅 핸들러 추가
+    stream_handler = logging.StreamHandler()
+
+    current_app.logger.info("======================")
+    current_app.logger.info(form)
+    current_app.logger.info(msg)
 
 @api.route('/test', methods= ['POST', 'GET'])
 def test():
     diet = models.get_diet('test0000')
-    return diet
+    tool(diet)
+    return jsonify({
+                "success":"T",
+                "message":200,
+                "data":diet,
+            }
+        )
 
         
