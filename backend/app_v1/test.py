ca13ca00eb0fcfb3d85c1d8e6d09ea41fd03bc54
yunyechan9893 <dpcks9893@naver.com>
2023-07-12 14:01:26 +0900

    Test: 식단, 먹었던 음식 select 기능 구현 후 테스트

--- backend/app_v1/test.py
+++ backend/app_v1/test.py
@@ -17,13 +17,43 @@ def tool(msg, form=''):
 
 @api.route('/test', methods= ['POST', 'GET'])
 def test():
-    diet = models.get_diet('test0000')
-    tool(diet)
-    return jsonify({
-                "success":"T",
-                "message":200,
-                "data":diet,
+    diets = models.get_diet('test0000')
+    diet_numbers = [diet.get_no() for diet in diets]
+    ate_food = models.get_ate_food(diet_numbers)
+
+    diet_list = []
+    for idx, diet in enumerate(diets):
+        food_list = [
+            {
+                "name": food.get_food_name(),
+                "amount": food.get_amount(),
+                "kcal": food.get_kcal(),
+                "carbohydrate": food.get_carbohydrate(),
+                "protein": food.get_protein(),
+                "fat": food.get_fat(),
+                "sodium": food.get_sodium()
             }
-        )
+            for food in ate_food
+            if diet.get_no() == food.get_diet_no()
+        ]
+
+        diet_info = {
+            "no": idx + 1,
+            "type_of_meal": diet.get_type_of_meal(),
+            "meal_time": diet.get_meal_time(),
+            "comment": diet.get_comment(),
+            "date": diet.get_date(),
+            "url": diet.get_url(),
+            "food": food_list
+        }
+        diet_list.append(diet_info)
+
+    response_data = {
+        "success": "T",
+        "message": 200,
+        "data": diet_list
+    }
+
+    return jsonify(response_data)
 
         
