84913155c185d502bc1ea30f029dc5ae6fc67aa7
yunyechan9893 <dpcks9893@naver.com>
2023-07-12 16:09:48 +0900

    Style: 코드 최적화

--- backend/app_v1/login.py
+++ backend/app_v1/login.py
@@ -10,6 +10,12 @@ def login():
     user_id  = params['user_id']
     user_pwd = params['user_pwd']
 
+    response_data = {
+            "success":"F", 
+            "message":100, 
+            "token":""
+    }
+
     result = models.login_defualt(user_id, user_pwd)
     if result :
         user_id = result.get_id()
@@ -28,13 +34,23 @@ def login():
         redis.set( refresh_token, json.dumps(refresh_token_info, ensure_ascii=False).encode('utf-8') )
 
         diets = models.get_diet(user_id)
-        diet_numbers = [diet.get_no() for diet in diets]
-        ate_food = models.get_ate_food(diet_numbers)
+        
+        if diets:
+            diet_numbers = [diet.get_no() for diet in diets]
+            diet_numbers_dict = {diet_numbers[i]:i for i in range(len(diet_numbers))}
+            ate_food = models.get_ate_food(diet_numbers)
 
-        diet_list = []
-        for idx, diet in enumerate(diets):
+            diet_info = [ {
+                "no": diet_numbers_dict.get(diet.get_no()) ,
+                "type_of_meal": diet.get_type_of_meal(),
+                "meal_time": diet.get_meal_time(),
+                "comment": diet.get_comment(),
+                "date": diet.get_date(),
+                "url": diet.get_url()} for diet in diets] 
+
             food_list = [
                 {
+                    "diet_no": diet_numbers_dict.get(food.get_diet_no()),
                     "name": food.get_food_name(),
                     "amount": food.get_amount(),
                     "kcal": food.get_kcal(),
@@ -44,37 +60,18 @@ def login():
                     "sodium": food.get_sodium()
                 }
                 for food in ate_food
-                if diet.get_no() == food.get_diet_no()
             ]
 
-            diet_info = {
-                "no": idx + 1,
-                "type_of_meal": diet.get_type_of_meal(),
-                "meal_time": diet.get_meal_time(),
-                "comment": diet.get_comment(),
-                "date": diet.get_date(),
-                "url": diet.get_url(),
-                "food": food_list
+            response_data = {
+                    "success":"T",
+                    "message":200,
+                    "refresh_token":refresh_token,
+                    "access_token":access_token,
+                    "diet_info":diet_info,
+                    "food_list":food_list
             }
-            
-            diet_list.append(diet_info)
 
-        response_data = {
-                "success":"T",
-                "message":200,
-                "refresh_token":refresh_token,
-                "access_token":access_token,
-                "diet_data":diet_list
-        }
-
-
-        return jsonify(response_data)
-    
-    return jsonify({
-            "success":"F", 
-            "message":100, 
-            "token":""
-        })
+    return jsonify(response_data)
 
 @api.route('/login/token', methods= ['POST'])
 def login_token():
