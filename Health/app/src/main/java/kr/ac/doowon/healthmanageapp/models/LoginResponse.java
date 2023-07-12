d8719040ed344f23d2756b6de9cffce5b01cedbb
yunyechan9893 <dpcks9893@naver.com>
2023-07-12 18:43:23 +0900

    Feat: getDietInfo, getAteFoodList 함수 구현

--- Health/app/src/main/java/kr/ac/doowon/healthmanageapp/models/LoginResponse.java
+++ Health/app/src/main/java/kr/ac/doowon/healthmanageapp/models/LoginResponse.java
@@ -2,6 +2,8 @@ package kr.ac.doowon.healthmanageapp.models;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
 public class LoginResponse {
     @SerializedName("success")
     private boolean success;
@@ -12,7 +14,11 @@ public class LoginResponse {
     @SerializedName("access_token")
     private String access_token;
 
+    @SerializedName("diet_info")
+    private List diet_info;
 
+    @SerializedName("food_list")
+    private List food_list;
 
 
     public boolean isSuccess() {
@@ -27,5 +33,11 @@ public class LoginResponse {
     }
     public String getRefreshToken() {
         return refresh_token;
+    }
+    public List getDietInfo() {
+        return diet_info;
+    }
+    public List getAteFoodList() {
+        return food_list;
     }
 }
