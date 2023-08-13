35774a2a6b668ad19a8558ed900c66ffd0ce79cd
yunyechan9893 <dpcks9893@naver.com>
2023-08-14 00:00:59 +0900

    Feat: Retrofit call 통신에서 Rxjava Single로 변경

--- Health/app/src/main/java/kr/ac/doowon/healthmanageapp/models/RetrofitInterface.java
+++ Health/app/src/main/java/kr/ac/doowon/healthmanageapp/models/RetrofitInterface.java
@@ -1,21 +1,26 @@
 package kr.ac.doowon.healthmanageapp.models;
 
+import io.reactivex.rxjava3.core.Single;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface RetrofitInterface {
         @POST("login")
-        Call<LoginResponse> login(@Body UserRequest request);
-
-        @POST("signup")
-        Call<JsonResponese> signup(@Body UserRequest request);
+        Single<LoginResponse> login(@Body LoginRequest request);
 
         @POST("login/token")
         Call<JsonResponese> tokenLogin(@Body LoginTokenRequest request);
+
+        @GET("login/diet/{id}")
+        Single<DietResponese> getDiet( @Path("id") String id );
+
+        @GET("login/target-kcal/{id}")
+        Single<TargetKcalResponese> getTargetKcal( @Path("id") String id );
+        @POST("signup")
+        Call<JsonResponese> signup(@Body UserRequest request);
 
         @GET("duplicate-check/id/{id}")
         Call<JsonResponese> checkDuplicateId(@Path("id") String id);
