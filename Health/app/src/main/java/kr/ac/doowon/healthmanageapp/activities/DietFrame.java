d3d8e64068ef811fbea50e58717ceb56743e98e9
yunyechan9893 <dpcks9893@naver.com>
2023-08-21 22:54:49 +0900

    Refactor: DialogFragment 코드 변경에 따른 코드 변경

--- Health/app/src/main/java/kr/ac/doowon/healthmanageapp/activities/DietFrame.java
+++ Health/app/src/main/java/kr/ac/doowon/healthmanageapp/activities/DietFrame.java
@@ -1,12 +1,20 @@
 package kr.ac.doowon.healthmanageapp.activities;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import kr.ac.doowon.healthmanageapp.fragments.dialog.MyEditTextDialog;
 import kr.ac.doowon.healthmanageapp.fragments.management.DietAdd;
@@ -19,7 +27,6 @@ import kr.ac.doowon.healthmanageapp.fragments.dialog.MyDatePicker;
 import kr.ac.doowon.healthmanageapp.view_model.NavigationVIewModel;
 
 public class DietFrame extends AppCompatActivity implements View.OnClickListener {
-
     ActivityDietBinding binding;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,10 +48,10 @@ public class DietFrame extends AppCompatActivity implements View.OnClickListener
         binding.dietViewpager.setUserInputEnabled(false);
         binding.dietViewpager.setCurrentItem(1);
 
-        binding.ibtnDate.setOnClickListener(this::onClick);
-        binding.ibtnDietRegister.setOnClickListener(this::onClick);
-        binding.ibtnHome.setOnClickListener(this::onClick);
-        binding.ibtnTargetKcal.setOnClickListener(this::onClick);
+        binding.ibtnDate.setOnClickListener(this);
+        binding.ibtnDietRegister.setOnClickListener(this);
+        binding.ibtnHome.setOnClickListener(this);
+        binding.ibtnTargetKcal.setOnClickListener(this);
     }
 
     @Override
@@ -57,15 +64,51 @@ public class DietFrame extends AppCompatActivity implements View.OnClickListener
             binding.dietViewpager.setCurrentItem(2);
         }
         else if (binding.ibtnDate.equals(v)){
-            DialogFragment dialog = new MyDatePicker();
-            dialog.show(this.getSupportFragmentManager(), "tag");
+
+            String dateString = "23.08.21 10:10:10";
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yy HH:mm:ss");
+
+            try {
+                Date date = dateFormat.parse(dateString);
+                long timeInMillis = date.getTime();
+
+                MyDatePicker dialog = MyDatePicker.getInstance(timeInMillis);
+                dialog.setPositiveClick(() -> {
+                    // 날짜를 선택시 나옴
+                });
+                dialog.show(this.getSupportFragmentManager(), "tag");
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+
         }
         else  if (binding.ibtnHome.equals(v)){
             moveDietFragment();
         }
         else if (binding.ibtnTargetKcal.equals(v)){
-            MyEditTextDialog dialog = new MyEditTextDialog();
-            dialog.setTitle("목표 칼로리").setContentHint("목표 Kcal를 입력해주세요");
+
+            // DialogFragment.show() will take care of adding the fragment
+            // in a transaction.  We also want to remove any currently showing
+            // dialog, so make our own transaction and take care of that here.
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            Fragment prev = getSupportFragmentManager().findFragmentByTag("dialog");
+            if (prev != null) {
+                ft.remove(prev);
+            }
+            ft.addToBackStack(null);
+
+            String title = "목표 칼로리";
+            String contentHint = "목표 Kcal를 입력해주세요";
+            MyEditTextDialog dialog = MyEditTextDialog.getInstance(title, contentHint);
+            dialog.setPositiveClick(() -> {
+                        // 목표칼로리 Insert, 같은 날짜에 값이 있다면 Update
+
+            });
+            dialog.setNegativeClick(() -> Log.i("1", "이게되네2?"));
+
+            dialog.show(ft, "dialog");
         }
     }
 
